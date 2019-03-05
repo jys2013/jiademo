58d6cdc16687b79fd2639ee11d08dda42d7f048b
jys2013 <jiayisheng2011@163.com>
2019-03-05 14:15:14 +0800

    修改项目名称

---
+++ service-feign/src/main/java/com/jiayisheng/sericefeign/SchedualServiceHiHystric.java
@@ -0,0 +1,12 @@
+package com.jiayisheng.sericefeign;
+
+import com.jiayisheng.sericefeign.client.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
--- service-ribbon/src/main/java/com/jiayisheng/serviceribbon/controller/HelloController.java
+++ service-ribbon/src/main/java/com/jiayisheng/serviceribbon/controller/HelloController.java
@@ -1,7 +1,10 @@
 package com.jiayisheng.serviceribbon.controller;
 
 import com.jiayisheng.serviceribbon.service.HelloService;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,4 +18,6 @@ public class HelloController {
     public String hi(@RequestParam String name){
         return helloService.hiService(name);
     }
+
+
 }
