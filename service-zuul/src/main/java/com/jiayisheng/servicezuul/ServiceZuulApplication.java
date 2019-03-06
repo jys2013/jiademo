41d0c31e70950d403ef83effae5a3fe9ea3fa083
jys2013 <jiayisheng2011@163.com>
2019-03-06 09:36:55 +0800

    添加config-server

---
+++ service-zuul/src/main/java/com/jiayisheng/servicezuul/ServiceZuulApplication.java
@@ -0,0 +1,25 @@
+package com.jiayisheng.servicezuul;
+
+import com.fasterxml.jackson.core.filter.TokenFilter;
+import filter.MyFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceZuulApplication.class, args);
+    }
+
+    @Bean
+    public MyFilter tokenFilter() {
+        return new MyFilter();
+    }
+
+}
